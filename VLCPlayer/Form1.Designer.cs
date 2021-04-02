2795ca42d1dc4bd2ec7da76fd546fe05a2b634ba
BacaSys <dombacy@gmail.com>
2021-04-02 15:32:57 +0200

    Dodaj pliki projektów.

---
+++ VLCPlayer/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace VLCPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoView1 = new LibVLCSharp.WinForms.VideoView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openURIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fullscreenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // videoView1
+            // 
+            this.videoView1.BackColor = System.Drawing.Color.Black;
+            this.videoView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoView1.Location = new System.Drawing.Point(0, 24);
+            this.videoView1.MediaPlayer = null;
+            this.videoView1.Name = "videoView1";
+            this.videoView1.Size = new System.Drawing.Size(800, 426);
+            this.videoView1.TabIndex = 0;
+            this.videoView1.Text = "videoView1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.openURIToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // openURIToolStripMenuItem
+            // 
+            this.openURIToolStripMenuItem.Name = "openURIToolStripMenuItem";
+            this.openURIToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openURIToolStripMenuItem.Text = "Open URI";
+            this.openURIToolStripMenuItem.Click += new System.EventHandler(this.openURIToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fullscreenToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // fullscreenToolStripMenuItem
+            // 
+            this.fullscreenToolStripMenuItem.Name = "fullscreenToolStripMenuItem";
+            this.fullscreenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fullscreenToolStripMenuItem.Text = "Fullscreen";
+            this.fullscreenToolStripMenuItem.Click += new System.EventHandler(this.fullscreenToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.videoView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LibVLCSharp.WinForms.VideoView videoView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openURIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fullscreenToolStripMenuItem;
+    }
+}
+
---
+++ VLCPlayer/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibVLCSharp.Shared;
+
+namespace VLCPlayer
+{
+    public partial class Form1 : Form
+    {
+
+        public LibVLC _libVLC;
+        public MediaPlayer _mp;
+        public Media media;
+
+        public bool isFullscreen = false;
+        public bool isPlaying = false;
+        public Size oldVideoSize;
+        public Size oldFormSize;
+        public Point oldVideoLocation;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Core.Initialize();
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(ShortcutEvent);
+            oldVideoSize = videoView1.Size;
+            oldFormSize = this.Size;
+            oldVideoLocation = videoView1.Location;
+
+            _libVLC = new LibVLC();
+            _mp = new MediaPlayer(_libVLC);
+            videoView1.MediaPlayer = _mp;
+        }
+
+        public void ShortcutEvent(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.F && !isFullscreen)
+            {
+                fullscreenToolStripMenuItem_Click(sender, e);
+            }
+
+            if(e.KeyCode == Keys.Escape  && isFullscreen)
+            {
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                this.WindowState = FormWindowState.Normal;
+                this.Size = oldFormSize;
+
+                menuStrip1.Visible = true;
+                videoView1.Size = oldVideoSize;
+                videoView1.Location = oldVideoLocation;
+                isFullscreen = false;
+            }
+
+            if(isPlaying)
+            {
+                if(e.KeyCode == Keys.Space)
+                {
+                    if (_mp.State == VLCState.Playing)
+                        _mp.Pause();
+                    else
+                        _mp.Play();
+                }
+            }
+
+            if (e.KeyCode == Keys.J)
+                _mp.Position -= .01f;
+            if (e.KeyCode == Keys.L)
+                _mp.Position += .01f;
+        }
+
+        public void PlayFile(string file)
+        {
+            _mp.Play(new Media(_libVLC, file));
+            isPlaying = true;
+        }
+
+        public void PlayURI(string file)
+        {
+            _mp.Play(new Media(_libVLC, new Uri(file)));
+            isPlaying = true;
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            if (fd.ShowDialog() == DialogResult.OK)
+                PlayFile(fd.FileName);
+        }
+
+        private void openURIToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 url_fd = new Form2();
+            url_fd.Show();  
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void fullscreenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            menuStrip1.Visible = false;
+            videoView1.Size = this.Size;
+            videoView1.Location = new Point(0, 0);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            isFullscreen = true;
+        }
+    }
+}
